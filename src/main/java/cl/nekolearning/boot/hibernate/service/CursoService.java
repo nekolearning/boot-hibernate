482bec93e602ce1b5f47587b51f6f0f66af285fa
Esteban Peralta Galarce <estebanalejandro@gmail.com>
2019-04-01 21:59:11 -0300

    capa servicio

---
+++ src/main/java/cl/nekolearning/boot/hibernate/service/CursoService.java
@@ -0,0 +1,23 @@
+package cl.nekolearning.boot.hibernate.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.nekolearning.boot.hibernate.dao.ICursoDAO;
+import cl.nekolearning.boot.hibernate.entity.Curso;
+
+@Service
+public class CursoService implements ICursoService {
+	
+	@Autowired
+	private ICursoDAO cursoDAO;
+
+	@Override
+	public List<Curso> recuperarCursos() {
+		
+		return cursoDAO.recuperaCursos();
+	}
+
+}
---
+++ src/main/java/cl/nekolearning/boot/hibernate/service/ICursoService.java
@@ -0,0 +1,11 @@
+package cl.nekolearning.boot.hibernate.service;
+
+import java.util.List;
+
+import cl.nekolearning.boot.hibernate.entity.Curso;
+
+public interface ICursoService {
+	
+	List<Curso> recuperarCursos();
+
+}
