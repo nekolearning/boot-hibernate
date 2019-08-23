06d0bd0f6db4ca0005edef9403c9184886a0910e
Esteban Peralta Galarce <estebanalejandro@gmail.com>
2019-08-22 23:06:56 -0400

    another commit

--- src/main/java/cl/nekolearning/boot/hibernate/controller/IndexController.java
+++ src/main/java/cl/nekolearning/boot/hibernate/controller/IndexController.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import cl.nekolearning.boot.hibernate.entity.Curso;
 import cl.nekolearning.boot.hibernate.service.ICursoService;
 
+//@Controller, @Service y @Repository son hijos de @Component, cada uno con capacidades específicas para su capa respectiva.
+
 @Controller
 public class IndexController {
 
+	//@Autowired indica a Spring que este bean se encuentra en su container.
 	@Autowired
+	//@Qualifier indica el nombre del bean que queremos obtener.
 	@Qualifier("curso")
 	private ICursoService cursoService;
 
@@ -31,5 +35,7 @@ public class IndexController {
 		Curso curso = cursoService.recuperarCursoPorId(1);
 		return curso;
 	}
+	
+	
 
 }
